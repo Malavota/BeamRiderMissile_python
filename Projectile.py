9ed91ccd1600dde6ae8256b7bc016ae5f0032e25
Malavota <pedromalavota@gmail.com>
2026-05-13 21:23:21 -0300

    Discard sketching files

--- Projectile.py
+++
@@ -1,9 +0,0 @@
-class Projectile():
-    def __init__(self):
-        self.mission_complete = False
-        self.alive = True
-        self.expl_rad = 2.0
-    
-    def destroy_target(self):
-        self.alive = False
-        self.mission_complete = True
